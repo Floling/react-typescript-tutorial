6d138524d20cab7040fa4dc68db7b987ae55b1bb
Florian Soll <florian.soll@gmail.com>
2022-12-10 19:31:52 +0100

    create Section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Heading from "./components/Heading";
+import Section from "./components/Section";
 
 function App() {
     return (
         <>
             <Heading title={"Hello"} />
+            <Section title={"Different Title"}>This is my Section</Section>
         </>
     );
 }
